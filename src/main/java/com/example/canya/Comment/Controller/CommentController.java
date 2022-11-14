915dd79c6d7bf391b1432067a6ee967af876033f
Jaylin16 <jaylin6115@gmail.com>
2022-11-14 20:20:29 +0900

    [FEAT] [subject: Comment] 댓글 생성, 전체조회, 삭제, 수정 기능 추가 [해결]

--- src/main/java/com/example/canya/Comment/Controller/CommentController.java
+++ src/main/java/com/example/canya/Comment/Controller/CommentController.java
@@ -1,9 +1,43 @@
 package com.example.canya.Comment.Controller;
 
+import com.example.canya.Comment.Dto.CommentRequestDto;
+import com.example.canya.Comment.Service.CommentService;
+import com.example.canya.Member.Service.MemberDetailsImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
+
+    private final CommentService commentService;
+
+    //댓글 생성.
+    @PostMapping("/auth/comment/{boardId}/create")
+    public ResponseEntity<?> createComment (@PathVariable Long boardId, @RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal MemberDetailsImpl memberDetails) {
+        return commentService.createComment(boardId, commentRequestDto, memberDetails.getMember());
+    }
+
+    //댓글 전체 조회
+    @GetMapping("/get/comments")
+    public ResponseEntity<?> commentList () {
+        return commentService.getCommentList();
+    }
+
+    //댓글 삭제
+    @DeleteMapping("/auth/comment/{commentId}/delete")
+    public ResponseEntity<?> commentDelete(@PathVariable Long commentId, @AuthenticationPrincipal MemberDetailsImpl memberDetails) {
+        return commentService.deleteComment(commentId, memberDetails.getMember());
+    }
+
+
+    //댓글 수정
+    @PutMapping("/auth/comment/{commentId}/update")
+    public ResponseEntity<?> commentUpdate(@PathVariable Long commentId, @RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal MemberDetailsImpl memberDetails) {
+        return commentService.commentUpdate(commentId, commentRequestDto, memberDetails.getMember());
+    }
+
+
 }
--- src/main/java/com/example/canya/Comment/Repository/CommentRepository.java
+++ src/main/java/com/example/canya/Comment/Repository/CommentRepository.java
@@ -1,7 +1,9 @@
 package com.example.canya.Comment.Repository;
 
+import com.example.canya.Comment.Entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
