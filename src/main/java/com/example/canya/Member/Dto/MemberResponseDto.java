a8ae1bb431f5da16299e4b07bce122ed6e13af30
Jaylin16 <jaylin6115@gmail.com>
2022-11-17 05:51:37 +0900

    [FEAT] [subject: Member(mypage)] 내가 작성한 게시글 전체조회 기능 추가 [해결]

--- src/main/java/com/example/canya/Member/Dto/MemberResponseDto.java
+++ src/main/java/com/example/canya/Member/Dto/MemberResponseDto.java
@@ -2,21 +2,38 @@ package com.example.canya.Member.Dto;
 
 import com.example.canya.Board.Entity.Board;
 import com.example.canya.Comment.Entity.Comment;
+import com.example.canya.Image.Entity.Image;
+import com.example.canya.Rating.Entity.Rating;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
+//@JsonInclude(JsonInclude.Include.NON_NULL) // null값 제외한 나머지 값만 반환하는 어노테이션.
 public class MemberResponseDto {
 
+    private Long boardId;
     private String boardTitle;
     private String boardContent;
     private String commentContent;
+    private String address;
+    private Integer commentCount;
+    private Double totalRating;
+    private List<Rating> ratingList = new ArrayList<>();
+    private List<Image> imageList = new ArrayList<>();
 
 
     public MemberResponseDto(Board board) {
+        this.boardId = board.getBoardId();
         this.boardTitle = board.getBoardTitle();
         this.boardContent = board.getBoardContent();
+        this.address = board.getAddress();
+        this.totalRating = board.getRatingList().get(ratingList.size()).getTotalRating();
+        this.imageList = board.getImageList();
+        this.commentCount = board.getCommentList().size();
     }
 
     public MemberResponseDto(Comment comment) {
