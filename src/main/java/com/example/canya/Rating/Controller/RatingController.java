570bdd0361410a3934b8b54b9c3c16d107f15567
Jaylin16 <jaylin6115@gmail.com>
2022-11-20 02:59:17 +0900

    [FEAT] [subject: Swagger] Swagger 추가 [해결]

--- src/main/java/com/example/canya/Rating/Controller/RatingController.java
+++ src/main/java/com/example/canya/Rating/Controller/RatingController.java
@@ -1,9 +1,9 @@
 package com.example.canya.Rating.Controller;
 
-
-import com.example.canya.Board.Entity.Board;
 import com.example.canya.Rating.Dto.RatingRequestDto;
 import com.example.canya.Rating.Service.RatingService;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "Rating (별점)")
 public class RatingController {
 
     private final RatingService ratingService;
 
+    @Operation(summary = "게시물별 별점 추가", description = "각 게시물별 별점 생성 기능")
     @PostMapping("/auth/rating/{boardId}")
     public ResponseEntity<?> rating(@RequestBody RatingRequestDto dto, @PathVariable Long boardId){
         System.out.println("boardId = " + boardId);
