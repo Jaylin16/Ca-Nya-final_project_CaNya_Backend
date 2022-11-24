4e4caf3d6c781433dddbcf58b6dd2ea918ee83ef
Jaylin16 <jaylin6115@gmail.com>
2022-11-25 02:35:55 +0900

    [FEAT] [subject: Member(mypage)] 마이페이지 게시글, 댓글 생성 일자 추가 [해결]

--- src/main/java/com/example/canya/Member/Dto/MemberResponseDto.java
+++ src/main/java/com/example/canya/Member/Dto/MemberResponseDto.java
@@ -3,25 +3,31 @@ package com.example.canya.Member.Dto;
 import com.example.canya.Board.Entity.Board;
 import com.example.canya.Comment.Entity.Comment;
 import com.example.canya.Image.Entity.Image;
+import com.example.canya.Member.Entity.Member;
 import com.example.canya.Rating.Entity.Rating;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
-
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
-//@JsonInclude(JsonInclude.Include.NON_NULL) // null값 제외한 나머지 값만 반환하는 어노테이션.
+@JsonInclude(JsonInclude.Include.NON_NULL) // null값 제외한 나머지 값만 반환하는 어노테이션.
 public class MemberResponseDto {
 
     private Long boardId;
+    private Long memberId;
     private String boardTitle;
+    private String memberNickname;
     private String boardContent;
     private String address;
+    private String boardCreatedAt;
     private Long commentId;
     private String commentContent;
     private Integer commentCount;
+    private String commentCreatedAt;
     private String totalRating;
     private Integer heartCount;
     private List<Rating> ratingList = new ArrayList<>();
@@ -32,6 +38,7 @@ public class MemberResponseDto {
         this.boardId = board.getBoardId();
         this.boardTitle = board.getBoardTitle();
         this.boardContent = board.getBoardContent();
+        this.boardCreatedAt = board.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         this.address = board.getAddress();
         this.totalRating = String.format("%.1f",board.getRatingList().get(ratingList.size()).getTotalRating());
         this.imageList = board.getImageList();
@@ -45,5 +52,12 @@ public class MemberResponseDto {
         this.boardContent = comment.getBoard().getBoardContent();
         this.commentId = comment.getCommentId();
         this.commentContent = comment.getCommentContent();
+        this.commentCreatedAt = comment.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
+
+    public MemberResponseDto(Member member){
+        this.memberId =member.getMemberId();
+        this.memberNickname = member.getMemberNickname();
+    }
+
 }
